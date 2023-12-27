553494f2468a90b8e45a2f0ed2acc1c4f88a4685
GrinkoTS <grinkots@yandex.ru>
2023-12-27 10:20:43 +0300

    Добавила новые страница и картинки

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,26 @@
 from flask import render_template
 from app import app
+import datetime
 
 @app.route('/index')
 @app.route('/')
 def index():
-    my_value = 'МИР'
-    return render_template('index.html', my_value = my_value)
-    #return '<h1> Привет, мир! </h1> <p> В этом параграфе я выведу <a href="https://yandex.ru/"> ссылку на яндекс </a> </p>'
+    now = datetime.datetime.now()
+
+    day_period = {'morning': 'Доброе утро!',
+                  'day': 'Добрый день!',
+                  'evening': 'Добрый вечер!',
+                  'night': 'Доброй ночи!'}
+
+    products = ['Шапки', 'Шарфы', 'Чепчики', 'Кардиганы', 'Жилетки', 'Носочки', 'Пледы']
+
+    return render_template('index.html', date=now, day_period=day_period, products=products)
+
+
+@app.route('/Шапки')
+def hats():
+    return render_template('hats.html')
+
+@app.route('/Шарфы')
+def scarves():
+    return render_template('scarves.html')
